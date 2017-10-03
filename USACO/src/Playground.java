9b530d656f53888cdc5a7b92430967b729ee75b2
Eldrojin <dystraxus@gmail.com>
2017-10-03 15:56:45 -0500

    Start Beads

---
+++ USACO/src/Playground.java
@@ -0,0 +1,9 @@
+public class Playground {
+    public static void main(String[] args) {
+        long a = 1;
+        double b = 1;
+        int c = 1;
+        float d = 1f;
+
+    }
+}
---
+++ USACO/src/beads.java
@@ -0,0 +1,18 @@
+/*
+ID: 18nguye1
+LANG: JAVA
+PROG: beads
+*/
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class beads {
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("beads.in"));
+        StringTokenizer in = new StringTokenizer(f.readLine());
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("beads.out")));
+
+
+    }
+}
