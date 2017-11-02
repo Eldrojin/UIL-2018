a47ee0a69997f9687e535af5d769f8d5e4ac0a31
Eldrojin <dystraxus@gmail.com>
2017-11-02 11:50:01 -0500

    Continue Cleanup

--- UIL 2018 Juan Seguin/src/Cleanup.java
+++ UIL 2018 Juan Seguin/src/Cleanup.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cleanup {
+    static int hours = 0;
     public static void main(String[] args) throws IOException{
         Scanner in = new Scanner(new File("cleanup.dat"));
         while(in.hasNextLine())
@@ -13,19 +14,17 @@ public class Cleanup {
             line = line.replaceAll(",", " ");
             line = line.replaceAll(" -", "");
             String[] input = line.split(" ");
+            hours = 0;
             int container = new Integer(input[input.length - 1]);
             int[] path = new int[input.length - 1];
             for (int i = 0; i < path.length; i++) {
                 path[i] = new Integer(input[i]);
             }
-            System.out.println(cleanup(path, container, container, 0, 0));
+            System.out.println(hours);
         }
     }
 
-    public static int cleanup(int[] path, int maxContain, int container, int hours, int pos) {
-        if (container == maxContain) {
-            cleanup(path, maxContain, container, hours + 1, pos - 1);
-        }
+    public static void cleanup(int[] path, int maxContain, int container, int pos) {
         if (pos == 0) {
             boolean full = false;
             for (int i = 0; i < path.length; i++) {
@@ -33,16 +32,17 @@ public class Cleanup {
                     full = true;
                 }
             }
-            if (container == maxContain) {
-                container = 0;
+            if (container <= 0) {
+                container = maxContain;
                 if (full) {
-                    cleanup(path, maxContain, container, hours + 1, pos + 1);
+                     cleanup(path, maxContain, container,pos + 1);
+                     hours+=2;
                 }
             }
             if (full) {
-                cleanup(path, maxContain, container, hours + 1, pos + 1);
+                 cleanup(path, maxContain, container,pos + 1);
+                 hours++;
             }
-            return hours;
         }
         if (pos == path.length - 1) {
             if (path[pos] > 0) {
@@ -51,16 +51,19 @@ public class Cleanup {
                     path[pos] += -container;
                     container = maxContain;
                 }
-                cleanup(path, maxContain, container, hours + 2, pos - 1);
+                 cleanup(path, maxContain, container, pos - 1);
+                hours++;
             } else if (pos > 0) {
                 if (path[pos] > 0) {
                     container -= path[pos];
                     if (container < 0) {
                         path[pos] += -container;
                         container = maxContain;
-                        cleanup(path, maxContain, container, hours + 2, pos - 1);
+                         cleanup(path, maxContain, container, pos - 1);
+                         hours++;
                     }
-                    cleanup(path, maxContain, container, hours + 1, pos + 1);
+                     cleanup(path, maxContain, container,pos + 1);
+                    hours++;
                 }
             }
         }
