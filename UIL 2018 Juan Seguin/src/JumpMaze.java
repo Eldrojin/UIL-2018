79992ee57eb00743309cbaa67dae77b85e74a397
Eldrojin <dystraxus@gmail.com>
2017-12-18 11:42:57 -0600

    Finish JumpMaze

--- UIL 2018 Juan Seguin/src/JumpMaze.java
+++ UIL 2018 Juan Seguin/src/JumpMaze.java
@@ -1,57 +1,59 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class JumpMaze {
-    public static void main(String[] args) throws IOException{
-        Scanner in = new Scanner(new File("jump_maze.dat"));
-        while(in.hasNextLine())
-        {
-            String [] dimensions = in.nextLine().split("x");
-            char[][]map = new char[new Integer(dimensions[0])][new Integer(dimensions[1])];
-            for (int i = 0; i < new Integer(dimensions[1]); i++) {
-                map[i] = in.nextLine().toCharArray();
-            }
-            int x = 0, y = 0;
+    static boolean b = false;
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner in = new Scanner(new File("judge/jump_maze.dat"));
+        while (in.hasNextLine()) {
+            b = false;
+            String[] dimList = in.nextLine().split("x");
+            int xS = 0, yS = 0;
+            char[][] map = new char[new Integer(dimList[1])][new Integer(dimList[0])];
             for (int i = 0; i < map.length; i++) {
+                char[] line = in.nextLine().toCharArray();
                 for (int j = 0; j < map[i].length; j++) {
-                    if(map[i][j] == 'S')
-                    {
-                        x=i; y = j;
+                    map[i][j] = line[j];
+                    if (map[i][j] == 'S') {
+                        yS = i;
+                        xS = j;
                     }
                 }
             }
-            System.out.println(maze(map, x, y)? "keep" : "scrap");
+            seek(map, xS, yS);
+            System.out.println(b ? "keep" : "scrap");
         }
     }
-    public static boolean maze(char[][]map, int x, int y)
-    {
-        if(x < map.length && x >= 0 && y < map[0].length && y>=0)
-        {
-            char temp = map[x][y];
-            map[x][y] = 'X';
-            if(temp == 'S' || temp == '-')
-            {
-                maze(map, x-1, y);
-                maze(map, x+1, y);
-                maze(map, x, y+1);
-                maze(map, x, y-1);
+
+    public static void seek(char[][] map, int x, int y) {
+        if (y >= 0 && y < map.length && x >= 0 && x < map[y].length && map[y][x] != 'V' && map[y][x] != 'W') {
+//           Scanner in = new Scanner(System.in);
+//            for (char[] chars : map) {
+//                for (char aChar : chars) {
+//                    System.out.print(aChar);
+//                }
+//                System.out.println();
+//            }
+//            in.nextLine();
+            if (map[y][x] == 'E') {
+                b = true;
+                return;
             }
-            if(temp == 'J')
-            {
-                maze(map, x-2, y);
-                maze(map, x+2, y);
-                maze(map, x, y+2);
-                maze(map, x, y-2);
+            if (map[y][x] == 'J') {
+                map[y][x] = 'V';
+                seek(map, x + 2, y);
+                seek(map, x - 2, y);
+                seek(map, x, y + 2);
+                seek(map, x, y - 2);
+            } else {
+                map[y][x] = 'V';
+                seek(map, x + 1, y);
+                seek(map, x - 1, y);
+                seek(map, x, y + 1);
+                seek(map, x, y - 1);
             }
-            if(temp == 'E')
-            {
-                return true;
-            }
-            map[x][y] = temp;
         }
-        return false;
     }
-
-
 }
